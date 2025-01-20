dfd12644533d01f98f649c02c8e3a54bfa8b6e9b
nacht29 <nacht29.study@gmail.com>
2025-01-20 15:20:18 +0800

    feat: cleaned runner_orders table for Case 2

--- pizza_runner/cleaning.sql
+++ pizza_runner/cleaning.sql
@@ -1,24 +1,68 @@
-CREATE TEMPORARY TABLE IF NOT EXISTS tmp_customer_order AS 
+-- clean customer_orders table
+-- exclusions/extras are toppings to remove/add to pizzas
+CREATE TEMPORARY TABLE IF NOT EXISTS tmp_customer_order AS
 SELECT
 	order_id,
 	customer_id,
 	pizza_id,
 	CASE
-		WHEN exclusions IS NULL
-		OR LOWER(exclusions) = 'null' 
-		OR exclusions = ''
-		THEN ' '
-		ELSE exclusions
-	END AS exclusions,
+		WHEN exclusions IS NULL OR exclusions LIKE 'null'
+			THEN ' '
+		ELSE
+			exclusions
+		END AS exclusions,
 	CASE
-		WHEN extras IS NULL
-		OR LOWER(extras) = 'null'
-		OR extras = ''
-		THEN ' '
-		ELSE extras
+		WHEN extras IS NULL OR extras LIKE 'null'
+			THEN ' '
+		ELSE
+			extras
 	END AS extras,
-	CAST(order_time AS DATETIME) AS order_time
+	order_time
 FROM
 	customer_orders;
+-- SELECT * FROM tmp_customer_order;
 
-SELECT * FROM tmp_customer_order;
+
+-- cleam runner_orders table
+-- replaces null str and NULL val as ' '
+-- double TRIM to trim 'minutes' wtc, then trim the space
+--		e.g. 13 minute$ -> 13 $ -> 13$
+CREATE TEMPORARY TABLE tmp_runner_order AS
+SELECT
+	order_id,
+	runner_id,
+	CASE
+		WHEN pickup_time IS NULL OR pickup_time LIKE 'null' 
+			THEN ' '
+		ELSE
+			pickup_time
+		END AS pickup_time,
+	CASE
+		WHEN distance IS NULL OR distance LIKE 'null'
+			THEN ' '
+		WHEN distance LIKE '%km' OR distance LIKE '% km'
+			THEN TRIM(TRIM('km' FROM distance))
+		ELSE
+			distance
+		END AS distance,
+	CASE
+		WHEN duration IS NULL OR duration LIKE 'null'
+			THEN ' '
+		WHEN duration LIKE '%mins' OR duration LIKE '% mins'
+			THEN TRIM(TRIM('mins' FROM duration))
+		WHEN duration LIKE '%minute' OR duration LIKE '% minute'
+			THEN TRIM(TRIM('minute' FROM duration))
+		WHEN duration LIKE '%minutes' OR duration LIKE '% minutes'
+			THEN TRIM(TRIM('minutes' FROM duration))
+		ELSE
+			duration
+		END AS duration,
+	CASE
+		WHEN cancellation IS NULL OR cancellation LIKE 'null'
+			THEN ' '
+		ELSE
+			cancellation
+		END AS cancellation
+FROM
+	runner_orders;
+-- SELECT * FROM tmp_runner_order;
