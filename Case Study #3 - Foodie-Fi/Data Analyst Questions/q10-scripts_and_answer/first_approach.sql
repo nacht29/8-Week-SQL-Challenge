1542c430591f5f7e62fc3c632fd70ffb55a30d3e
nacht29 <nacht29.study@gmail.com>
2025-02-12 15:07:14 +0800

    feat: revised q9 and q10

--- Case Study #3 - Foodie-Fi/Data Analyst Questions/q10-scripts_and_answer/first_approach.sql
+++ Case Study #3 - Foodie-Fi/Data Analyst Questions/q10-scripts_and_answer/first_approach.sql
@@ -1,38 +1,37 @@
 WITH trial_plan AS (
-	-- Get customers subscribed to the trial plan
-	SELECT 
-		customer_id, 
+	SELECT
+		customer_id,
 		start_date AS trial_date
-	FROM foodie_fi.subscriptions
+	FROM subscriptions
 	WHERE plan_id = 0
 ),
 
 annual_plan AS (
-	-- Get customers subscribed to the pro annual plan
-	SELECT 
-		customer_id, 
+	SELECT
+		customer_id,
 		start_date AS annual_date
-	FROM foodie_fi.subscriptions
-	WHERE plan_id = 3
+	FROM subscriptions
+	WHERE plan_id = 4
 ),
 
+-- create and assign a bucket index to each customer
+-- the index corresponds to a specifc range
+-- say idx = 0, range = 1-30 days
 bucket AS (
-	-- Create 30-day buckets based on days taken to upgrade
-	SELECT 
-		FLOOR((annual.annual_date - trial.trial_date) / 30) AS avg_days_to_upgrade
+	SELECT
+		FLOOR((annual_date - trial_date) / 30) AS bucket_idx
 	FROM trial_plan AS trial
 	JOIN annual_plan AS annual
 		ON trial.customer_id = annual.customer_id
 )
-  
-SELECT 
+
+SELECT
 	CONCAT(
-		avg_days_to_upgrade * 30, 
-		' - ', 
-		(avg_days_to_upgrade + 1) * 30 - 1, 
-		' days'
-	) AS bucket, 
-	COUNT(*) AS num_of_customers
+		bucket_idx * 30, -- start range (index starts at 0, hence day 0)
+		' - ',
+		(bucket_idx + 1) * 30 - 1 -- end range [(multiple of 30) - 1]
+	) AS day_range,
+	COUNT(*) AS customers_count
 FROM bucket
-GROUP BY avg_days_to_upgrade
-ORDER BY avg_days_to_upgrade;
+GROUP BY bucket_idx
+ORDER BY bucket_idx;
--- Case Study #3 - Foodie-Fi/Data Analyst Questions/q10-scripts_and_answer/second_approach.sql
+++ Case Study #3 - Foodie-Fi/Data Analyst Questions/q10-scripts_and_answer/second_approach.sql
@@ -1,38 +1,37 @@
 WITH trial_plan AS (
-	-- Get customers subscribed to the trial plan
-	SELECT 
-		customer_id, 
+	SELECT
+		customer_id,
 		start_date AS trial_date
-	FROM foodie_fi.subscriptions
+	FROM subscriptions
 	WHERE plan_id = 0
 ),
 
 annual_plan AS (
-	-- Get customers subscribed to the pro annual plan
-	SELECT 
-		customer_id, 
+	SELECT
+		customer_id,
 		start_date AS annual_date
-	FROM foodie_fi.subscriptions
-	WHERE plan_id = 3
+	FROM subscriptions
+	WHERE plan_id = 4
 ),
 
-buckets AS (
-	-- Create 30-day buckets based on days taken to upgrade
-	SELECT 
-		FLOOR((annual.annual_date - trial.trial_date - 1) / 30) AS bucket_index
+-- create and assign a bucket index to each customer
+-- the index corresponds to a specifc range
+-- say idx = 0, range = 1-30 days
+bucket AS (
+	SELECT
+		FLOOR((annual_date - trial_date - 1) / 30) AS bucket_idx
 	FROM trial_plan AS trial
 	JOIN annual_plan AS annual
 		ON trial.customer_id = annual.customer_id
 )
-  
-SELECT 
+
+SELECT
 	CONCAT(
-		bucket_index * 30 + 1,  -- Start of the bucket
-		'-', 
-		(bucket_index + 1) * 30, -- End of the bucket
-		' days'
-	) AS bucket, 
-	COUNT(*) AS num_of_customers
-FROM buckets
-GROUP BY bucket_index
-ORDER BY bucket_index;
+		bucket_idx * 30 + 1, -- start range (since idx starts at 0, we add 1 to get 1-30 etc)
+		' - ',
+		(bucket_idx + 1) * 30 -- end range (always a multiple of 30)
+	) AS day_range,
+	COUNT(*) AS customers_count
+FROM bucket
+GROUP BY bucket_idx
+ORDER BY bucket_idx;
